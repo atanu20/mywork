70662a70b14efb88b9cead66f486e8a2f9c669dc
atanu <atanuj383@gmail.com>
2022-07-21 01:40:17 +0530

    upload file

--- src/data/fdata.js
+++ src/data/fdata.js
@@ -1,4 +1,3 @@
-export const apilink = 'http://localhost:5000';
-// // export const socketlink = 'http://localhost:9000';
-// export const socketlink = 'https://moon-socket.herokuapp.com';
-// export const apilink = 'https://moonn-jobs.herokuapp.com';
+// export const apilink = 'http://localhost:5000';
+
+export const apilink = 'https://bill-manage-aj.herokuapp.com';
