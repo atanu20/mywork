7338c1aed8c4fdbdeff52f44bdb1eceb623fe9f8
atanu <atanuj383@gmail.com>
2022-11-22 12:47:32 +0530

    Add

--- src/data/fdata.js
+++ src/data/fdata.js
@@ -1,3 +1,4 @@
 // export const apilink = 'http://localhost:5000';
 
-export const apilink = 'https://bill-manage-aj.herokuapp.com';
+// export const apilink = 'https://bill-manage-aj.herokuapp.com';
+export const apilink = 'https://bill-server.onrender.com';
